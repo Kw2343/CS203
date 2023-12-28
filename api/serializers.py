9152139ee7912bc070e154a47a08bd5856ec1d82
kw2343 <107032802+Kw2343@users.noreply.github.com>
2023-12-28 22:11:45 +1300

    Add files via upload

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import *
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'description', 'photo')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'name','projectmanager', 'start_date','end_date', 'comment', 'status')
+
+
+   
+class ProjectManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectManager
+        fields = ('name', 'id')
